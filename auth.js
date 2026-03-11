8b7a5931fa09c0b2909c3a9c91642915bec328eb
MWIZERWA K. Bertrand <mkbertrand123@gmail.com>
2026-03-11 14:53:33 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,12 +1,7 @@
-// auth.js
-// Hardcoded password - stored externally for security
-// Password: karekezi@2 (encoded as char codes to avoid plain text)
 (function() {
   window.AUTH = {
-    // Encoded password - not easily visible
     getPassword: function() {
-      // karekezi@2 in char codes
-      const encoded = [107, 97, 114, 101, 107, 101, 122, 105, 64, 50];
+      const encoded = [98, 101, 114, 105, 118, 101, 108, 101, 110, 97, 64, 49, 50];
       return String.fromCharCode(...encoded);
     },
     
