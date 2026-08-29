60c7a44a3b455c1f5909f8b3b2c546a053f82769
MWIZERWA K. Bertrand <mkbertrand123@gmail.com>
2026-08-29 22:11:43 +0200

    Create Icon.jsx

---
+++ src/components/Icon.jsx
@@ -0,0 +1,8 @@
+// ============================================================
+// FILE: src/components/Icon.jsx
+// ============================================================
+import React from 'react';
+
+export const Icon = ({ icon, className = '', style = {} }) => (
+  <iconify-icon icon={icon} class={className} style={style}></iconify-icon>
+);
