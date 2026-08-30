a460550fcce092c2aad294d7054316a18d722e3c
MWIZERWA K. Bertrand <mkbertrand123@gmail.com>
2026-08-30 07:00:25 +0200

    Update CredentialsWidget.jsx

--- src/widgets/CredentialsWidget.jsx
+++ src/widgets/CredentialsWidget.jsx
@@ -1,6 +1,4 @@
-// ============================================================
-// FILE: src/widgets/CredentialsWidget.jsx
-// ============================================================
+ 
 import React, { useState, useMemo } from 'react';
 import { Icon } from '../components/Icon';
 import { useModal } from '../context/ModalContext';
