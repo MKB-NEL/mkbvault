8e5ccedbfd21f0c72032c0d6de337e4c87080624
MWIZERWA K. Bertrand <mkbertrand123@gmail.com>
2026-08-30 06:59:43 +0200

    Update Project.jsx

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,6 +1,4 @@
-// ============================================================
-// FILE: src/pages/Project.jsx
-// ============================================================
+ 
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate, useSearchParams } from 'react-router-dom';
 import { Icon } from '../components/Icon';
